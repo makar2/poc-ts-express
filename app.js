934f0122db90d289613674e501a27176539883f4
Makar <makar.melikyan@gmail.com>
2021-08-16 04:02:12 +0400

    minor change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import createError from 'http-errors';
 import express from 'express';
 import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import { dirname, join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
